f38342b6fc6e7a98c8539cd58c9c2cf9652b053b
Yoonthe <chinayuzaiy@outlook.com>
2019-05-04 23:08:09 +0800

    添加colorcube

---
+++ colorCube.js
@@ -0,0 +1,8 @@
+                                                                                           
+class ColorCell {
+  constructor() {
+    this.redCount = 0;
+    this.blueCount = 0;
+    this.yellowCount = 0;
+  }
+}
