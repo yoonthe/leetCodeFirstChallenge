9f1b225e8ac2ac277d5860be38d9ea7259ae0946
yoonthe <yuzaiyang.yoonthe@bytedance.com>
2021-06-08 14:12:48 +0800

    fix: update solution

--- 76.最小覆盖子串.js
+++ 76.最小覆盖子串.js
@@ -12,7 +12,7 @@ k/*
  */
 var minWindow = function(s, t) {
   const resolveStr = {}, max = s.length;
-  let i, j, k, res = '', len = 0;
+  let i, j, k, res = '', len = 0, temp;
   for(i = 0; i< t.length; i++) {
     if (t[i] in resolveStr) {
       resolveStr[t[i]]--;
@@ -21,8 +21,52 @@ var minWindow = function(s, t) {
       len--;
     }
   }
-  // console.log(JSON.stringify(resolveStr));
+  // const provideStr = [];
+  // for (i = 0 ; i < s.length; i++) {
+  //   if (s[i] in resolveStr) {
+  //     provideStr.push([s[i], i]);
+  //   }
+  // }
+
+  // // console.log(JSON.stringify(resolveStr));
   i = 0; j = t.length - 1;
+  // for (k = i; k <= j; k++) {
+  //   temp = provideStr[k][0];
+  //   if (temp in resolveStr) {
+  //     resolveStr[temp]++;
+  //     if (resolveStr[temp] === 0) {
+  //       len++;
+  //     }
+  //   }
+  // }
+  // // console.log(JSON.stringify(resolveStr));
+  // if (len === 0) {
+  //   return s.slice(provideStr[i][1], provideStr[j][1] + 1);
+  // }
+  // while (j < provideStr.length) {
+  //   j++;
+  //   // push j
+  //   if (s[j] in resolveStr) {
+  //     resolveStr[s[j]]++;
+  //     if(resolveStr[s[j]] === 0) {
+  //       len++;
+  //     }
+  //   }
+  //   while(len === 0 && i <= j) {
+  //     // got an answer, pop i
+  //     if (s[i] in resolveStr) {
+  //       resolveStr[s[i]]--;
+  //       if (resolveStr[s[i]] < 0) {
+  //         if (res.length === 0 || j - i + 1 < res.length) {
+  //           // update answer
+  //           res = s.slice(i, j + 1);
+  //         }
+  //         len--;
+  //       }
+  //     }
+  //     i++;
+  //   }
+  // }
   for (k = i; k <= j; k++) {
     if (s[k] in resolveStr) {
       resolveStr[s[k]]++;
@@ -35,21 +79,16 @@ var minWindow = function(s, t) {
   if (len === 0) {
     return s.slice(i, j + 1);
   }
-  while(1) {
-    while (len < 0 && j < max) {
-      j++;
-      // push j
-      if (s[j] in resolveStr) {
-        resolveStr[s[j]]++;
-        if(resolveStr[s[j]] === 0) {
-          len++;
-        }
+  while (j < max) {
+    j++;
+    // push j
+    if (s[j] in resolveStr) {
+      resolveStr[s[j]]++;
+      if(resolveStr[s[j]] === 0) {
+        len++;
       }
     }
-    if (j >= max) {
-      break;
-    }
-    while (len === 0 && i < max) {
+    while(len === 0 && i <= j) {
       // got an answer, pop i
       if (s[i] in resolveStr) {
         resolveStr[s[i]]--;
@@ -62,9 +101,6 @@ var minWindow = function(s, t) {
         }
       }
       i++;
-    }
-    if (i >= max) {
-      break;
     }
   }
   return res;
